5b58dfa757547186cfa4fe656f18e1ff4748ee7f
John Scarrott <johnpscarrott7777@hotmail.com>
2012-11-20 14:37:42 +0000

    Updated the user class

--- EncryptSync/src/core/User.java
+++ EncryptSync/src/core/User.java
@@ -6,4 +6,18 @@ public class User {
 	String name;
 	Directory inputDirectory;
 	Directory outputDirectory;
+	
+	User(String nameIn, String in, String out){
+		name = nameIn;
+		inputDirectory = new Directory( in);
+		outputDirectory = new Directory( out);
+	}
+
+	public String getPasswordKey() {
+		return passwordKey;
+	}
+
+	public void setPasswordKey(String passwordKey) {
+		this.passwordKey = passwordKey;
+	}
 }

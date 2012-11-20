a33cb732034c08960f25dc0ba859b7428f647123
John Scarrott <johnpscarrott7777@hotmail.com>
2012-11-20 20:37:51 +0000

    Updates to junits, some modification to User class

---
+++ EncryptSync/src/test/userTest.java
@@ -0,0 +1,26 @@
+package test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import core.User;
+
+public class userTest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@Test
+	public void test() {
+		User testUser = new User("John", "C:\\Users\\Home\\git\\EncryptSync\\EncryptSync", "C:\\Users\\Home\\git\\EncryptSync\\EncryptSync");
+		assertEquals("John", testUser.getName());
+		assertEquals( "C:\\Users\\Home\\git\\EncryptSync\\EncryptSync", testUser.getInputDirectory().getLocation().toString());
+		assertEquals( "C:\\Users\\Home\\git\\EncryptSync\\EncryptSync", testUser.getOutputDirectory().getLocation().toString());	
+		
+	
+	}
+
+}
